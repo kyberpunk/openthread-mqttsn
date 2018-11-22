8cf08069475cad4cdeef4c3da00d3dd4b0456aa0
kyberpunk <vit.holasek@siemens.com>
2018-11-22 13:49:05 +0100

    Build fixes

--- source/mqttsn_client.cpp
+++ source/mqttsn_client.cpp
@@ -21,7 +21,6 @@ MqttsnClient::MqttsnClient(Instance& aInstance)
 }
 
 static int32_t PacketDecode(unsigned char* data, size_t length) {
-	int rc = MQTTSNPACKET_READ_ERROR;
 	int lenlen = 0;
 	int datalen = 0;
 
@@ -91,43 +90,47 @@ otError MqttsnClient::Stop() {
 
 otError MqttsnClient::Connect(MqttsnConfig &config) {
 	otError error = OT_ERROR_NONE;
+	Ip6::MessageInfo messageInfo;
+	char* clientIdString = nullptr;
+	MQTTSNPacket_connectData options = MQTTSNPacket_connectData_initializer;
+	int32_t length = 0;
+	Message *message = nullptr;
+
 	if (mIsConnected) {
 		error = OT_ERROR_INVALID_STATE;
-		goto error;
+		goto exit;
 	}
 
-	char* clientIdString = new char[config.GetClientId().length() + 1];
+	clientIdString = new char[config.GetClientId().length() + 1];
 	if (clientIdString == nullptr) {
 		error = OT_ERROR_FAILED;
 	}
-	strcpy(clientId.cstring, config.GetClientId().c_str());
-	MQTTSNPacket_connectData options = MQTTSNPacket_connectData_initializer;
 	MQTTSNString clientId;
+	strcpy(clientId.cstring, config.GetClientId().c_str());
 	clientId.cstring = clientIdString;
 	options.clientID = clientId;
 	options.duration = config.GetDuration();
 	options.cleansession = static_cast<unsigned char>(config.GetCleanSession());
 
 	unsigned char buffer[MAX_PACKET_SIZE];
-	int32_t length = MQTTSNSerialize_connect(buffer, MAX_PACKET_SIZE, &options);
+	length = MQTTSNSerialize_connect(buffer, MAX_PACKET_SIZE, &options);
 	delete[] clientIdString;
 	if (length <= 0) {
 		error = OT_ERROR_FAILED;
-		goto error;
+		goto exit;
 	}
 
 	otMessageSettings settings;
 	settings.mLinkSecurityEnabled = false;
 	settings.mPriority = OT_MESSAGE_PRIORITY_NORMAL;
-	Message *message = mSocket.NewMessage(length, &settings);
+	message = mSocket.NewMessage(length, &settings);
 	if (message == nullptr) {
 		error = OT_ERROR_FAILED;
-		goto error;
+		goto exit;
 	}
 	message->Write(0, length, buffer);
 	message->SetOffset(0);
 
-	Ip6::MessageInfo messageInfo;
 	messageInfo.SetPeerAddr(config.GetAddress());
 	messageInfo.SetPeerPort(config.GetPort());
 	messageInfo.SetInterfaceId(OT_NETIF_INTERFACE_ID_THREAD);
